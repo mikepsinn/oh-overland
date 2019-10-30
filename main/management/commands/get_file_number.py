614fffac430ae132196a2b48001450c3d441a7e7
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2019-10-30 15:43:24 +0100

    check management task

--- main/management/commands/get_file_number.py
+++ main/management/commands/get_file_number.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand
 from openhumans.models import OpenHumansMember
+from celery import current_app
 
 
 class Command(BaseCommand):
     help = 'Process so far unprocessed data sets'
 
     def handle(self, *args, **options):
+        print(current_app.control.inspect().active_queues())
         oh_members = OpenHumansMember.objects.all()
         for oh_member in oh_members:
             file_number = len(oh_member.list_files())
