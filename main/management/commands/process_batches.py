c46f35074938e27c6d442c784d1b72fea92dc37a
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2021-06-01 15:57:45 +0200

    no fail on all batches if token invalid

--- main/management/commands/process_batches.py
+++ main/management/commands/process_batches.py
@@ -9,11 +9,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         oh_members = OpenHumansMember.objects.all()
         for oh_member in oh_members:
-            files = oh_member.list_files()
-            batches = []
-            for f in files:
-                if f['basename'].startswith('overland-batch-'):
-                    batches.append(f['basename'])
-            batches.sort()
-            for f in batches:
-                process_batch.delay(f, oh_member.oh_id)
+            try:
+                files = oh_member.list_files()
+                batches = []
+                for f in files:
+                    if f['basename'].startswith('overland-batch-'):
+                        batches.append(f['basename'])
+                batches.sort()
+                for f in batches:
+                    process_batch.delay(f, oh_member.oh_id)
+            except:
+                pass
